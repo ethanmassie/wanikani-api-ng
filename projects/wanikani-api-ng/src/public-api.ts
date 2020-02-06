45cb39b77874a8a39c05e0e3189545c21786120a
Ethan Massie <ethan.massie1@gmail.com>
2020-02-05 20:20:44 -0600

    removed old exports

--- projects/wanikani-api-ng/src/public-api.ts
+++ projects/wanikani-api-ng/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of wanikani-api-ng
  */
 
-export * from './lib/wanikani-api-ng.service';
-export * from './lib/wanikani-api-ng.component';
+
 export * from './lib/wanikani-api-ng.module';
