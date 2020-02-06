a57de72f04edbc3310d4400d0ae932eacd0e4806
Ethan Massie <ethan.massie1@gmail.com>
2020-02-05 20:19:54 -0600

    added token services to providers list

--- projects/wanikani-api-ng/src/lib/wanikani-api-ng.module.ts
+++ projects/wanikani-api-ng/src/lib/wanikani-api-ng.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { WanikaniApiNgComponent } from './wanikani-api-ng.component';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenService } from './services/token.service';
 
 
 
 @NgModule({
-  declarations: [WanikaniApiNgComponent],
   imports: [
+    HttpClientModule,
   ],
-  exports: [WanikaniApiNgComponent]
+  providers: [
+    TokenService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }
+  ]
 })
 export class WanikaniApiNgModule { }
