89da821747f4765fa8983906180993536c98b9f4
Ethan Massie <ethan.massie1@gmail.com>
2020-02-08 16:56:47 -0600

    added assignments service to providers list

--- projects/wanikani-api-ng/src/lib/wanikani-api-ng.module.ts
+++ projects/wanikani-api-ng/src/lib/wanikani-api-ng.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { AssignmentsService } from './services/assignments.service';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [
+    AssignmentsService
   ]
 })
 export class WanikaniApiNgModule { }
