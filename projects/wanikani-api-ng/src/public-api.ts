9dd0d4bdb5f23c1bdd3328d09675e658df9d0b2e
Ethan Massie <ethan.massie1@gmail.com>
2020-02-08 17:50:20 -0600

    added voice actor models and service to exports

--- projects/wanikani-api-ng/src/public-api.ts
+++ projects/wanikani-api-ng/src/public-api.ts
@@ -9,6 +9,7 @@ export * from './lib/wanikani-api-ng.module';
 export * from './lib/enums/srs-stage-name.enum';
 export * from './lib/enums/srs-stage.enum';
 export * from './lib/enums/subject-type.enum';
+export * from './lib/enums/gender.enum';
 
 // Models
 export * from './lib/models/pages.model';
@@ -18,9 +19,13 @@ export * from './lib/models/assignment/assignment.model';
 // Level Progressions
 export * from './lib/models/level-progression/level-progression-collection.model';
 export * from './lib/models/level-progression/level-progression.model';
+// Voice Actor
+export * from './lib/models/voice-actor/voice-actor-collection.model';
+export * from './lib/models/voice-actor/voice-actor.model';
 
 // Services
 export * from './lib/services/assignments.service';
 export * from './lib/services/level-progressions.service';
 export * from './lib/services/token-interceptor.service';
+export * from './lib/services/voice-actor.service';
 export * from './lib/services/token.service';
