b9dafe98b374da2fd3772205856fa26a703ff055
Ethan Massie <ethan.massie1@gmail.com>
2020-02-08 16:57:03 -0600

    added exports

--- projects/wanikani-api-ng/src/public-api.ts
+++ projects/wanikani-api-ng/src/public-api.ts
@@ -2,5 +2,21 @@
  * Public API Surface of wanikani-api-ng
  */
 
-
+// Modules
 export * from './lib/wanikani-api-ng.module';
+
+// Enums
+export * from './lib/enums/srs-stage-name.enum';
+export * from './lib/enums/srs-stage.enum';
+export * from './lib/enums/subject-type.enum';
+
+// Models
+export * from './lib/models/pages.model';
+// Assignment
+export * from './lib/models/assignment/assignment-collection.model';
+export * from './lib/models/assignment/assignment.model';
+
+// Services
+export * from './lib/services/assignments.service';
+export * from './lib/services/token-interceptor.service';
+export * from './lib/services/token.service';
