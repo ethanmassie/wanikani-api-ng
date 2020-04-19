a623d65fa3999dd45f6f659d84de59b1bd5bfcdd
Ethan Massie <ethan.massie1@gmail.com>
2020-04-19 17:17:11 -0500

    added auth guard

---
+++ projects/wanikani-api-ng/src/lib/guards/auth.guard.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AuthGuard } from './auth.guard';
+import { WanikaniTokenService } from '../services/wanikani-token.service';
+
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        WanikaniTokenService
+      ]
+    });
+    guard = TestBed.inject(AuthGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
