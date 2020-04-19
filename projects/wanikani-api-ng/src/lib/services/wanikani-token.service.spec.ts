edc54dbfd4bc9ec859edf3bf9d26e0770fbac611
Ethan Massie <ethan.massie1@gmail.com>
2020-04-19 17:18:14 -0500

    added tests for isAuthenticated

--- projects/wanikani-api-ng/src/lib/services/wanikani-token.service.spec.ts
+++ projects/wanikani-api-ng/src/lib/services/wanikani-token.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WanikaniTokenService } from './wanikani-token.service';
+import { take } from 'rxjs/operators';
 
 describe('WanikaniTokenService', () => {
   let tokenService: WanikaniTokenService;
 
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [WanikaniTokenService]
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [WanikaniTokenService]
+    });
+    tokenService = TestBed.get(WanikaniTokenService);
+  });
 
   it('should be created', () => {
-    tokenService = TestBed.get(WanikaniTokenService);
     expect(tokenService).toBeTruthy();
   });
+
+  it('should be unauthenticated without a token available', () => {
+    tokenService.logout(); // logout in case there is already a token stored
+    tokenService.getIsAuthenticated().pipe(take(1)).subscribe(
+      authenticated => expect(authenticated).toBeFalsy()
+    );
+  })
+
+  it('should be authenticated when token is added', () => {
+    tokenService.setApiToken('any-token');
+    tokenService.getIsAuthenticated().pipe(take(1)).subscribe(
+      authenticated => expect(authenticated).toBeTruthy()
+    );
+  })
 });
