d68d6b4ca3e4b360cd4b2b25a2887a95068e195f
Ethan Massie <ethan.massie1@gmail.com>
2020-04-24 20:43:16 -0500

    fixed type of active in wani subscription

--- projects/wanikani-api-ng/src/lib/models/wani-subscription.model.ts
+++ projects/wanikani-api-ng/src/lib/models/wani-subscription.model.ts
@@ -1,7 +1,7 @@
 import { WaniSubscriptionType } from '../enums/wani-subscription-type.enum';
 
 export interface WaniSubscription {
-  active:             number;
+  active:             boolean;
   type:               WaniSubscriptionType;
   max_level_granted:  string;
   period_ends_at:     Date;
